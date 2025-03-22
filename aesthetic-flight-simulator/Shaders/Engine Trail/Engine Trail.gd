4e9e148962075296a544cf5cdbe89eefbe62bae2
HeofTheOttomans <145900606+HEofOttoman@users.noreply.github.com>
2025-03-22 23:45:17 +1100

    v0.27.0 -
    - Slightly tweaked the posture of the airplane on the ground
    - Got the engine trail working (only 1 half though)
    - Tried to add shooting mechanics; they do not work yet.

--- aesthetic-flight-simulator/Shaders/Engine Trail/Engine Trail.gd
+++ aesthetic-flight-simulator/Shaders/Engine Trail/Engine Trail.gd
@@ -8,7 +8,8 @@ func _ready():
 	mat = $MeshInstance3D.get_surface_override_material(0).duplicate()
 	$MeshInstance3D.set_surface_override_material(0, mat)
 
-func _on_Engine_update_interface(values):
+
+func _on_aircraft_module_engine_update_interface(values):
 	var power = values["engine_power"]
 	scale.z = 0.05+power
 	mat.albedo_color = lerp(Color.BLACK, Color.WHITE, power)
