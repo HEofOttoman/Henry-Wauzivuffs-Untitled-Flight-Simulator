15f31bb126f97737537cdc418b8ce10c84f3730d
HeofTheOttomans <145900606+HEofOttoman@users.noreply.github.com>
2025-04-04 23:27:06 +1100

    v0.34.0 - Version of Submission
    - Added new landing gear collision
    - Added a welcome message in the tutorial

--- aesthetic-flight-simulator/UI/Tutorial Billboard/welcome_message.gd
+++ aesthetic-flight-simulator/UI/Tutorial Billboard/welcome_message.gd
@@ -1,4 +1,4 @@
-extends SubViewport
+extends Label3D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +9,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _input(event):
+	if Input.is_key_pressed(KEY_X):
+		hide()
+		
+	if Input.is_key_pressed(KEY_Z):
+		show()
